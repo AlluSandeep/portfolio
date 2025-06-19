b501eb524518e247113d78f0a192c09864cf4007
Allu Sandeep <sandeepallu2004@gmail.com>
2025-06-19 18:33:32 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+//src/app/app.component.ts
+import { Component } from '@angular/core';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NgModel } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent, AboutComponent, ProjectsComponent, ContactComponent],
+  standalone: true,
+})
+export class AppComponent {}
